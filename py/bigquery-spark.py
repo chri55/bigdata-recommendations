be848952f46fb5bb35ad1a326db1e791ee9c1bb0
Chris Till <cpaultill@gmail.com>
2019-12-06 19:02:41 -0500

    algorithm mostly finished in py/test-recommendations-alg.py

--- py/bigquery-spark.py
+++ py/bigquery-spark.py
@@ -10,69 +10,57 @@ from pyspark.sql.functions import col
 print("IMPORT REQ")
 
 spark = (
-            SparkSession.builder.master("local")
-                .config("spark.io.compression.codec", "snappy")
-                .config("spark.ui.enabled", "false")
-                .config("spark.executor.memory", "16g")
-                .config("spark.driver.memory", "16g")
+            SparkSession.builder.master("yarn")
+                .config("spark.executor.memory", "10g")
+                .config("spark.driver.memory", "50g")
                 .config("spark.memory.offHeap.enabled",True)
                 .config("spark.memory.offHeap.size","16g")
                 .getOrCreate()
         )
 
-sc = SparkContext("gcp", "Recommender App")
-
 print("CREATE SPARK SESSION")
-# Use the Cloud Storage bucket for temporary BigQuery export data used
-# by the connector. This assumes the Cloud Storage connector for
-# Hadoop is configured.
-bucket = spark.sparkContext._jsc.hadoopConfiguration().get(
-    'fs.gs.system.bucket')
-spark.conf.set('temporaryGcsBucket', bucket)
-spark.conf.set('spark.sql.pivotMaxValues', u'367982')
-spark.conf.set('spark.driver.memory', u'15g')
 print("GET BUCKET SET SPARK CONFIGS")
 # Load data from BigQuery.
-ratings = spark.read.json('gs://als-output/als-ready-data.json')
+ratings = spark.read.json('gs://als-output/als-preprocessed-nonull/*.json')
 print("GET TABLE")
 ratings.createOrReplaceTempView('ratings')
-#ratings = ratings.repartition(6)
+#ratings = ratings.repartition(5)
 print("CREATE PARTITIONS")
 
-required_cols = ratings.select(ratings['reviewerID'], ratings['asin'], ratings['overall'])
-required_cols = required_cols.repartition(8)
+required_cols = ratings
 print("GET REQ'D COLS")
 
-indexer = [StringIndexer(inputCol = column, outputCol = column + "_index") for column in list(set(required_cols.columns) - set(['overall']))]
-print("CREATE STRINGINDEXER")
-pipeline = Pipeline(stages=indexer)
-print("CREATE PIPELINE")
-required_cols = pipeline.fit(required_cols).transform(required_cols)
-print("FIT_TRANSFORM STRINGINDEXER")
-required_cols.show()
-print("WRITING FILE...")
-required_cols.write.json("gs://als-output/als-preprocessed.json")
-print("FILE WRITTEN")
+#indexer = [StringIndexer(inputCol = column, outputCol = column + "_index") for column in list(set(required_cols.columns) - set(['overall', 'title']))]
+#print("CREATE STRINGINDEXER")
+#pipeline = Pipeline(stages=indexer)
+#print("CREATE PIPELINE")
+#required_cols = pipeline.fit(required_cols).transform(required_cols)
+#print("FIT_TRANSFORM STRINGINDEXER")
+#required_cols.show()
+
+#print("WRITING FILE...")
+#required_cols.write.json("gs://als-output/als-preprocessed-nonull")
+#print("FILE WRITTEN")
+
 (training, test) = required_cols.randomSplit([0.8, 0.2], 42)
 print("SPLIT DATA")
-als = ALS(maxIter=5, regParam=0.01, userCol='reviewerID_index', itemCol='asin_index', ratingCol='overall', coldStartStrategy="drop")
+als = ALS(maxIter=20, regParam=0.01, userCol='reviewerID_index', itemCol='asin_index', ratingCol='overall', coldStartStrategy="drop", checkpointInterval=5)
 print("CREATE ALS MODEL")
 als_model = als.fit(training)
+print("FITTED")
 print("FIT ALS MODEL")
 predictions = als_model.transform(test)
 print("TRANSFORM ALS MODEL")
-sc = spark.SparkContext
-als_model.save(sc, "gs://als-output/")
+als_model.save("gs://als-output/gg-no-re")
 print("SAVE MODEL")
 #evaluator = RegressionEvaluator(metricName="rmse", labelCol="overall", predictionCol="prediction")
 #rmse = evaluator.evaluate(predictions)
 #print("Root-mean-square-error =" + str(rmse))
 #predictions.show()
 
-recommendations = als_model.recommendProductsForUsers(5)
-print("CREATE RECOMMENDATIONS TABLE")
+#print("CREATE RECOMMENDATIONS TABLE")
 
-print("DATA WRITE START...")
+#print("DATA WRITE START...")
 # Saving the data to BigQuery
-recommendations.write.json('gs://als-output/recommendations.json')
-print("FINISHED")
+#recommendations.write.json('gs://als-output/recommendations.json')
+#print("FINISHED")
--- py/test-recommendations-alg.py
+++ py/test-recommendations-alg.py
@@ -1,82 +1,286 @@
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.ml.recommendation import ALS, ALSModel
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, rand
+from flask import Flask, request, render_template, render_template_string
 
-def getRecommendations(userReviews):
+
+def getRecommendations(userReviews, ratingtable):
     """
     Returns books based on what the user likes and dislikes
 
     list ratings: [("asin", "overall")]
     """
 
-    ratingtable = spark.read.json("gs://als-output/als-preprocessed.json/*.json")
 
     data_to_train = []
     book = 0
     for e in userReviews:
-        data_to_train.append(("asin_{}".format(book), e[0], e[1], "test_user", 2000000001))
-    df = spark.createDataFrame(data_to_train, ["asin", "asin_index", "overall", "reviewerID", "reviewerID_index"])
+        data_to_train.append(("asin_{}".format(book), e[0], e[1], "test_user", 2000000001, "fuckall"))
 
-    ratingtable = ratingtable.union(df)
+    df = spark.createDataFrame(data_to_train, ["asin", "asin_index", "overall", "reviewerID", "reviewerID_index", "title"])
 
-    als = ALS(maxIter=5, regParam=0.01, userCol='reviewerID_index', itemCol='asin_index', ratingCol='overall', coldStartStrategy="drop")
 
-    (training, test) = ratingtable.randomSplit([0.8, 0.2], 42)
+    '''
+        ratingtable = ratingtable.union(df)
 
-    model = als.fit(training)
-    model.transform(test)
-
-    recs = model.recommendForUserSubset(df, 5)
-
-    recList = recs.where(recs.reviewerID_index == 2000000001).select("recommendations").collect()
-
-    print(recList)
-
-    print("loading metadata")
-    meta = spark.read.json("gs://metabooks/*.json")
-
+        print("instance load")
+        als = ALS(maxIter=20, regParam=0.01, userCol='reviewerID_index', itemCol='asin_index', ratingCol='overall', coldStartStrategy="drop")
+        print("fit model")
+        als_model = als.fit(ratingtable)
+        #print("show preds")
+        #preds.show()
+    '''
+    print("recommend for user subset")
+    recs = als_model.recommendForUserSubset(df, 5)
 
     asins = []
 
-    print(recList[0].asDict()["recommendations"])
+    #print(recs)
+    print("collecting")
+    reclist = recs.where(recs.reviewerID_index == 2000000001).select("recommendations").limit(1).collect()
+    print(reclist)
+    print("collected dataframe")
 
-    for e in recList[0].asDict()["recommendations"]:
+    for e in reclist[0].asDict()["recommendations"]:
+        print("getting title")
         i = e.asDict(recursive=True)
-        asins.append(ratingtable.where(ratingtable.asin_index == int(i["asin_index"])).select("asin").collect())
+        print("got title")
+        asins.append(ratingtable.where(ratingtable.asin_index == int(i["asin_index"])).select("title").collect())
 
     print(asins)
 
-    titles = []
-
-    for e in asins:
-        i = e[0].asDict(recursive=True)
-        titles.append(meta.where(meta.asin == i["asin"]).select("title").collect())
-
-    print(titles)
-
-
-
+    return asins
 
 
 spark = (
             SparkSession.builder.master("yarn")
+                .config("spark.executor.memory", "10g")
+                .config("spark.driver.memory", "60g")
+                .config("spark.memory.offHeap.enabled",True)
+                .config("spark.memory.offHeap.size","16g")
                 .getOrCreate()
         )
 
-sc = spark.sparkContext
-print(sc.pythonVer)
-print(sc.version)
-
 print("CREATE SPARK SESSION")
-# Use the Cloud Storage bucket for temporary BigQuery export data used
-# by the connector. This assumes the Cloud Storage connector for
-# Hadoop is configured.
-bucket = spark.sparkContext._jsc.hadoopConfiguration().get(
-    'fs.gs.system.bucket')
-spark.conf.set('temporaryGcsBucket', bucket)
-spark.conf.set('spark.sql.pivotMaxValues', u'367982')
-spark.conf.set('spark.driver.memory', u'15g')
-print("GET BUCKET SET SPARK CONFIGS")
-l = [(10002, 5), (110661, 5), (104742, 2), (142626, 5)]
-g = getRecommendations(l)
+print("SET SPARK CONFIGS")
+
+#meta = spark.read.json("gs://metabooks/*.json")
+print("LOADING ratingtable")
+ratingtable = spark.read.json("gs://als-output/als-preprocessed-nonull/*.json")
+print("LOADED ratingtable")
+
+print("CACHING ratingtable")
+ratingtable.cache()
+print("CACHED ratingtable")
+
+print("LOADING metabooks")
+meta = spark.read.json("gs://metabooks/*.json")
+print("LOADED METABOOKS")
+
+print("CACHING  metabooks")
+meta.cache()
+print("CACHED metabooks")
+
+app = Flask(__name__)
+
+@app.route('/recommend', methods=['GET', 'POST'])  #can set first param to '/'
+def toyFunction():
+    global ratingtable
+    global meta
+
+    recommendHTML = '''
+        <!DOCTYPE html>
+        <html lang="en" dir="ltr">
+          <head>
+            <meta charset="utf-8">
+            <title>Book Recommendation Dashboard</title>
+            <style>
+                img {
+                    height: 200px;
+                    width: 100px;
+                }
+                p {
+                    font-size: 1.5em;
+                }
+            </style>
+          </head>
+          <body>
+            <h1>Recommended Book For You</h1>
+            <p> You gave these reviews: <p>
+            <ul>
+                {% for i in len %}
+                <li>{{ old_titles[i][0].title }} : <span style="color: red;">{{ old_titles[i][1].rating }}</span></li>
+                {% endfor %}
+            </ul>
+            <p>Based on other users with similar reviews, you might like</p>
+            <ul>
+                {% for i in len %}
+                <img src="{{ imgUrls[i][0].imUrl }}" alt="[no cover image found]" />
+                <li>{{ new_titles[i] }}</li>
+                {% endfor %}
+            </ul>
+            <br/>
+            <br/>
+            <a href="http://0.0.0.0:5001/">Back to Home Page</a>
+            <a href="http://0.0.0.0:5001/tables">See Analysis of All of the Amazon Data</a>
+          </body>
+        </html>
+    '''
+
+    if request.method == "POST":
+        result = request.form
+        print(type(result))
+        print(result.to_dict())
+        ratings = []
+        old_titles = []
+        for i in range(5):
+            ratings.append((float(result["book{}".format(i)]), int(result["rating{}".format(i)])))
+            old_titles.append((ratingtable.where(result["book{}".format(i)] == ratingtable.asin_index).select("title").limit(1).collect(), result["rating{}".format(i)]))
+        ret = []
+        #processed_data = getRecommendations(ratings, ratingtable)
+
+        ## MAIN PROCESS
+        data_to_train = []
+        old_indices = []
+        book = 0
+        for e in ratings:
+            old_indices.append(e[0])
+            data_to_train.append(("unknown_asin", e[0], e[1], "test_user", 2000000001, "random_title"))
+
+        df = spark.createDataFrame(data_to_train, ["asin", "asin_index", "overall", "reviewerID", "reviewerID_index", "title"])
+        df.show()
+        als_model = ALSModel.load("gs://als-output/gg-no-re")
+        preds = als_model.transform(df)
+        preds.show()
+        '''
+
+        ratingtable = ratingtable.union(df)
+
+        print("instance init")
+        als = ALS(maxIter=20, regParam=0.01, userCol='reviewerID_index', itemCol='asin_index', ratingCol='overall', coldStartStrategy="drop")
+        print("fit model")
+        als_model = als.fit(ratingtable)
+        #print("show preds")
+        #preds.show()
+        '''
+        print("recommend for user subset")
+        recs = als_model.recommendForUserSubset(df, 5)
+
+        asins = []
+        imgUrls = []
+
+        #print(recs)
+        print("collecting")
+        recs.show()
+        reclist = recs.where(recs.reviewerID_index in ).select("recommendations").limit(1).collect()
+        print(reclist)
+        print("collected dataframe")
+
+        for e in reclist[0].asDict()["recommendations"]:
+            print("getting title")
+            i = e.asDict(recursive=True)
+            print("got title")
+            asins.append(ratingtable.where(ratingtable.asin_index == int(i["asin_index"])).select("asin", "title").collect())
+
+
+        print(asins)
+
+        for e in asins:
+            if len(e[0].title) != 0:
+                ret.append(e[0].title)
+                imgUrls.append(meta.where(e[0].asin == meta.asin).select("imUrl").limit(1).collect())
+
+        print(old_titles, imgUrls)
+
+        return render_template_string(recommendHTML, new_titles=ret, old_titles=old_titles, imgUrls=imgUrls, len=range(len(ret)))
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    books = ratingtable.select("asin", "asin_index", "title").orderBy(rand()).limit(5).collect()
+
+    imgUrls = []
+    for i in range(len(books)):
+        imgUrls.append(meta.where(meta.asin == books[i].asin).select("imUrl").collect())
+        print(imgUrls[i][0].imUrl)
+
+    homeHTML = '''
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>Book Recommendation Dashboard</title>
+        <style>
+            img {
+                height: 200px;
+                width: 100px;
+            }
+            p {
+                font-size: 1.5em;
+            }
+        </style>
+      </head>
+      <body>
+        <h1>Type a book name or get random suggestions to rate.</h1>
+        <p> The application will recommend books it thinks you will like based on other similar users' experiences.</p>
+        <form action = "http://0.0.0.0:5001/recommend" method="POST">
+            {% for i in len %}
+                <p>
+                    <img src="{{ imgs[i][0].imUrl }}" alt="[no cover image found]"/>
+                    <br />
+                    <br />
+                    How would you rate: {{ books[i].title }} ?
+                    <input type="text" name="book{{ i }}" value={{ books[i].asin_index }} style="display:none;" />
+                    <select  name="rating{{ i }}">
+                        <option value="5" name="5">5</option>
+                        <option value="4" name="4">4</option>
+                        <option value="3" name="3">3</option>
+                        <option value="2" name="2">2</option>
+                        <option value="1" name="1">1</option>
+                    </select>
+
+                </p>
+            {% endfor %}
+            <input type="submit" value="Submit"/>
+        </form>
+        <br/>
+        <br/>
+        <a href="http://0.0.0.0:5001/tables">See Analysis of All of the Amazon Data</a>
+      </body>
+    </html>
+    '''
+    return render_template_string(homeHTML, books=books, len=range(len(books)), imgs=imgUrls)
+
+@app.route("/tables", methods=['GET'])
+def showTables():
+        tableHTML = '''
+        <!DOCTYPE html>
+        <html lang="en" dir="ltr">
+          <head>
+            <meta charset="utf-8">
+            <title>Book Recommendation Dashboard</title>
+            <style>
+                img {
+                    height: 200px;
+                    width: 100px;
+                }
+                p {
+                    font-size: 1.5em;
+                }
+            </style>
+          </head>
+          <body>
+            <iframe width="600" height="450" src="https://datastudio.google.com/embed/reporting/1uOxvMAwkfyJTOPE_shhDNGjFW_zbM0Bs/page/PaO8" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe width="700" height="525" src="https://datastudio.google.com/embed/reporting/12NAPAT-M0QaDGDKCgoXbtOO06Lb6N6ic/page/rXO8" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe width="600" height="450" src="https://datastudio.google.com/embed/reporting/13P6whnnaji7jAN-oLkwV1pxAMvI--BG8/page/MUO8" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe width="600" height="450" src="https://datastudio.google.com/embed/reporting/1cfRRAXzoc0shAJTkCDIm19aRHbC1_NRj/page/56U8" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <br/>
+            <br/>
+            <a href="http://0.0.0.0:5001/">Back to Home Page</a>
+          </body>
+        </html>
+        '''
+        return render_template_string(tableHTML)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001)
