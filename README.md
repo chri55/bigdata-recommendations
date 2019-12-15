2aa4bf51a225d15d6e7f335870d3fe346fe5e7be
Omkar <omkar.omdroid@gmail.com>
2019-12-15 14:07:47 -0500

    Added dashboard to flask app

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Big Data Algorithms (F19) Project to give Book Recommendations based on Amazon User Reviews
 Use 'source virtual/bin/activate' to start the environment included.
 Use 'python main.py' to run the test server.
+
+Server will start at http://127.0.0.1:8050/dashboard/. index.html is working. 
--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template, url_for
+import dash
+import dash_html_components as html
+from dashboard import draw_dashboard
+server = Flask(__name__)
 
-app = Flask(__name__)
-
-@app.route("/")
+@server.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/about")
+@server.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/recommend")
+@server.route("/recommend")
 def recommend():
     return render_template("recommendations.html")
 
+app = dash.Dash(
+    __name__,
+    server=server,
+    routes_pathname_prefix='/dashboard/'
+)
 
+draw_dashboard(app)
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run_server(debug=True)
